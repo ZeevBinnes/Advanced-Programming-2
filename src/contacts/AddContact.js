db7adf1fc4b2e3edb45c9c2d0540226fe392ece4
ZeevBinnes <92650073+ZeevBinnes@users.noreply.github.com>
2022-04-10 12:01:13 +0300

    added AddContact button - doesn't work yet

--- src/contacts/AddContact.js
+++ src/contacts/AddContact.js
@@ -1,6 +1,26 @@
 function AddContact() {
     return(
-        <div></div>
+        <div>
+            <button type="button" className="bi bi-person-plus rounded-pill" id="add_contact" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            </button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Add New Contact</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Modal body text goes here.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
